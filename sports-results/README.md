ed146c0e01115092f231a767e48e18da5266819f
Michael Ringholm Sundgaard <michaelringholm@gmail.com>
2017-06-19 09:44:16 +0200

    First checkin

---
+++ sports-results/README.md
@@ -0,0 +1,5 @@
+# Convert Eclipse project to a faceted project and add the Dynamic Web Module facet to enable deployment to Azure from within eclipse.
+
+# When running locally with Tomcat this page will be accessible via http://localhost:8080/stock-prices.
+
+# When running locally via Jetty this page will be accessible via http://localhost:8080/.
---
+++ sports-results/src/main/java/com/stelinno/microservice/controller/SportsResultsController.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.stelinno.microservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SportsResultsController {
+  @RequestMapping("/home")
+  public String home() {
+    return "Welcome to the Sports Results service";
+  }
+  
+  @RequestMapping("/football")
+  public String football() {
+    return "{ homeTeam:Brøndby, awayTeam:FCK, score:3-1 }";
+  }
+
+  /**
+   * <a href="https://cloud.google.com/appengine/docs/flexible/java/how-instances-are-managed#health_checking">
+   * App Engine health checking</a> requires responding with 200 to {@code /_ah/health}.
+   */
+  @RequestMapping("/_ah/health")
+  public String healthy() {
+    // Message body required though ignored
+    return "Still surviving.";
+  }
+
+}
